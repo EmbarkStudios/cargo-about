2589067890705818854f50bfd167cbfe43069dc3
Jake Shadle <jake.shadle@embark-studios.com>
2019-11-20 14:25:31 +0100

    Add licensee list

--- src/licenses/config.rs
+++ src/licenses/config.rs
@@ -31,6 +31,43 @@ where
     deserializer.deserialize_any(Visitor)
 }
 
+fn deserialize_licensee<'de, D>(
+    deserializer: D,
+) -> std::result::Result<Vec<spdx::Licensee>, D::Error>
+where
+    D: de::Deserializer<'de>,
+{
+    struct Visitor;
+
+    impl<'de> de::Visitor<'de> for Visitor {
+        type Value = Vec<spdx::Licensee>;
+
+        fn expecting(&self, formatter: &mut fmt::Formatter<'_>) -> fmt::Result {
+            formatter.write_str("array of SPDX licensees")
+        }
+
+        fn visit_seq<S>(self, mut seq: S) -> Result<Self::Value, S::Error>
+        where
+            S: de::SeqAccess<'de>,
+        {
+            let mut vec = Vec::new();
+
+            // Update the max while there are additional values.
+            while let Some(v) = seq.next_element()? {
+                let lic = spdx::Licensee::parse(v).map_err(|e| {
+                    de::Error::custom(format!("'{}' is not a valid SPDX licensee: {}", v, e))
+                })?;
+
+                vec.push(lic);
+            }
+
+            Ok(vec)
+        }
+    }
+
+    deserializer.deserialize_seq(Visitor)
+}
+
 #[derive(Deserialize, Debug)]
 #[serde(rename_all = "kebab-case")]
 pub struct Additional {
@@ -62,6 +99,9 @@ pub struct KrateConfig {
 
 #[derive(Deserialize, Debug, Default)]
 pub struct Config {
+    /// The list of licenses we will use for all crates, in priority order
+    #[serde(deserialize_with = "deserialize_licensee")]
+    pub accepted: Vec<spdx::Licensee>,
     #[serde(flatten)]
     pub inner: BTreeMap<String, KrateConfig>,
 }
