0ed0d1dbb88f2d33cf454f63c9c745c5801adc7f
Jake Shadle <jake.shadle@embark-studios.com>
2023-01-27 15:58:15 +0100

    Prep release (#215)
    * Update CHANGELOG
    
    * Update docs
    
    * Update dependencies
    
    * Back out toml version, broken

--- src/cargo-about/generate.rs
+++ src/cargo-about/generate.rs
@@ -42,9 +42,10 @@ pub struct Args {
     /// Scan licenses for the entire workspace, not just the active package
     #[clap(long)]
     workspace: bool,
-    /// Generate an error code when failing to read, synthesize, or clarify a license expression for a crate
+    /// Exit with a non-zero exit code when failing to read, synthesize, or
+    /// clarify a license expression for a crate
     #[clap(long)]
-    fail_on_missing_license: bool,
+    fail: bool,
     /// The template(s) or template directory to use. Must either be a `.hbs`
     /// file, or have at least one `.hbs` file in it if it is a directory
     templates: PathBuf,
@@ -191,12 +192,8 @@ pub fn cmd(args: Args, color: crate::Color) -> anyhow::Result<()> {
         .with_confidence_threshold(args.threshold)
         .gather(&krates, &cfg);
 
-    let (files, resolved) = licenses::resolution::resolve(
-        &summary,
-        &cfg.accepted,
-        &cfg.crates,
-        args.fail_on_missing_license,
-    );
+    let (files, resolved) =
+        licenses::resolution::resolve(&summary, &cfg.accepted, &cfg.crates, args.fail);
 
     use term::termcolor::ColorChoice;
 
